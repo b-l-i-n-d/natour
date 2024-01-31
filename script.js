782b406bbf98f40401f5c45c2ea0994fc1ff7a13
b-l-i-n-d <fahim.faisal.abir@gmail.com>
2024-01-31 15:45:29 +0600

    ✨feat: added haeder section with navigation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementById("demo").innerHTML = "Hello Dolly.";
+let navToggleCheckbox = document.querySelector('#nav-toggle');
+let navBtn = document.querySelector('.navigation__button');
+
+navBtn.addEventListener('click', function() {
+    if (navToggleCheckbox.checked) {
+        navToggleCheckbox.setChecked = false;
+    } else {
+        navToggleCheckbox.setChecked = true;
+    }
+});
